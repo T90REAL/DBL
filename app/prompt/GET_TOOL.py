af0412890d22d260bb2d0a3b7916d38f0a591e54
Xiao <muzumayao@126.com>
2025-08-03 21:13:07 +0800

    make prompts to be macro format.

---
+++ app/prompt/GET_TOOL.py
@@ -0,0 +1,5 @@
+GET_TOOL_PROMPT = """
+You have access to the following tools. You can choose one or more tools to use based on the user's goal and the conversation history. 
+Respond with a JSON object specifying the 'tool_name' and 'parameters'.\n\n
+Here are the tools you can use:\n
+"""
